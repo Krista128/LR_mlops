6efb9e21e6496f3c2a17d51fa243548d04c91eb5
Mikhail Minaev <singmesparrow@gmail.com>
2026-06-27 20:16:57 +0300

    feat: predictor-app \health test added

--- ci.bat
+++ ci.bat
@@ -8,3 +8,41 @@ flake8 src tests
 
 echo tests .. 
 pytest tests
+
+echo Build predictor app image ..
+docker build -t predictor-app:ci -f Dockerfile.predictor .
+if errorlevel 1 exit /b 1
+
+echo Run predictor app ..
+docker run -d --name predictor-app -p 8000:8000 predictor-app:ci
+if errorlevel 1 exit /b 1
+
+echo Wait for app ..
+
+set success=0
+
+for /L %%i in (1,1,10) do (
+    curl -f http://localhost:8000/health >nul 2>&1
+
+    if not errorlevel 1 (
+        set success=1
+        goto :done
+    )
+
+    timeout /t 2 /nobreak >nul
+)
+:done
+
+docker logs predictor-app
+echo Stop container ..
+docker rm -f predictor-app
+echo Delete image ..
+Docker rmi predictor-app:ci
+
+if "%success%"=="1" (
+    echo Health check passed.
+    exit /b 0
+)
+
+echo Health check failed.
+exit /b 1
